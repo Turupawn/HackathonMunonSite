bac155f26da3c7300600269840dfcd910ebc11fc
root <root@HackathonMunon.(none)>
2014-06-29 02:11:35 +0000

    testing secret key gitignore

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  file = File.open("/home/rails/facebook.secretkey", "rb")
+  file = File.open("facebook.secretkey", "rb")
   provider :facebook, '234879253380947', file.read.chomp
 end

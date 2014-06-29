6a406c388392e4d98d912b5a6248870cfcdd7397
root <root@HackathonMunon.(none)>
2014-06-29 02:08:24 +0000

    Security keys patch

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '234879253380947', 'a62e632861479525615ae7c02b9ba8fc'
+  file = File.open("/home/rails/facebook.secretkey", "rb")
+  provider :facebook, '234879253380947', file.read.chomp
 end
